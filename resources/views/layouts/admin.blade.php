55632138dac07915220cf802b1172998d0569941
”daiki1008” <ec2-user@ip-172-31-43-109.us-east-2.compute.internal>
2020-06-26 08:27:34 +0000

    PHP/Laravel12_課題提出

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,25 +24,51 @@
             {{-- 画面上部に表示するナビゲーションバーです。--}}
             <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
                 <div class="container">
+                    
                     <a class="navbar-brand" href="{{ url('/')}}">
                           {{  config('app.name','Laravel') }}
                     </a>
+                    
                     <button class="navbar=toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
+                        
                     <span class="navbar-toggler-icon"></span>
+                    
                     </button>
+                    
                     
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         
                         <ul class="navbar-nav mr-auto">
-                        </ul>
-                        
+                             </ul>
+                              
                         <ul class="navbar-nav ml-auto">
+                          @guest
+                          <li><a class="nav-link" href="{{route('login')}}">{{__('Login')}}</a></li>
+                            @else
+                        <li class="nav-item dropdown">
+                          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              {{ Auth::user()->name }} <span class="caret"></span></a>
+                              
+                              <div class="dropdown-menu" aria-labelledby="vavbarDropdown">
+                                    <a class="dropdown-item" href="{ route('logout')}" onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                                     {{__('Logout')}}
+                                    </a>
+                                    <form id="lgout-form" action="{{ route('logout')}}" method="POST" style="display: nome;">
+                                      @csrf
+                                    </form>      
+                               </div>
+                                </li>
+                                 @endguest
                         </ul>
                         
                     </div>
                 </div>
             </nav>
+            
+            
+            
 　　　　　　<main class="py-4">
 　　　　　　    @yield('content')
 　　　　　　</main>
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create','Admin\NewsController@add');
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('news/create','Admin\NewsController@add') -> middleware('auth');
+    Route::get('profile/create','Admin\ProfileController@add') -> middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit') -> middleware('auth');
 });
 
 /*課題提出 */
@@ -27,3 +27,7 @@ Route::group(['prefix' => 'admin'], function() {
 Route::get( 'XXX','Admin\AAAController@bbb');
 */
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
